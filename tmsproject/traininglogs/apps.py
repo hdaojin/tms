1cb08bfb7deec62af3b95fe377804d308337a470
hdaojin <hdaojin@hotmail.com>
2025-03-27 09:06:22 +0800

    将training APP名称修改为traininglogs

---
+++ tmsproject/traininglogs/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TraininglogsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'traininglogs'
+    verbose_name = '训练日志管理'
