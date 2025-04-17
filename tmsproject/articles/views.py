d56d135ddf8f35b200c57be38f0c818f90383c49
hdaojin <hdaojin@hotmail.com>
2025-04-17 16:31:57 +0800

    feat: 更新环境配置，添加日志上传目录和邀请码过期时间；重构文章模板，统一使用侧边栏布局；修复训练日志视图重定向问题

--- tmsproject/articles/views.py
+++ tmsproject/articles/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
-# from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Article
 # Create your views here.
-
 
 
 class ArticleListView(LoginRequiredMixin, ListView):
