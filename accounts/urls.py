2839a49d36a516f5021a1d0419a041f26eb438dc
hdaojin <hdaojin@hotmail.com>
2025-05-15 09:27:13 +0800

    调整模板结构，使其可以动态调整是否有侧边栏等

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,10 +4,18 @@ from .views import account_signup, account_profile, generate_invitation
 
 app_name = 'accounts'
 
+template_layout = {
+    "header": False,
+    "main": True,
+    "left_sidebar": False,
+    "right_sidebar": False,
+    "footer": True,
+}
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
         template_name='accounts/login.html',
-        extra_context={'title': '登录'},
+        extra_context={'title': '登录', 'template_layout': template_layout},
         ), name="login"),
     path('profile/', account_profile, name="profile"),
     path('logout/', auth_views.logout_then_login, name="logout"),
--- tmsproject/context_processors.py
+++ tmsproject/context_processors.py
@@ -7,11 +7,33 @@ from traininglogs.menus import get_training_log_menu_items
 
 
 def custom_context(request):
-    return {
+    """
+    自定义上下文处理器
+    """
+    site_info = {
         "my_site_name": settings.MY_SITE_NAME,
+    }
+
+    custom_menus = {
         "main_menu_items": main_menu_items,
         "user_menu_items": get_user_menu_items(request.user),
         "training_log_menu_items": get_training_log_menu_items(request.user),
         "page_menu_items": page_menu_items,
         "article_menu_items": article_menu_items,
     }
+
+    template_layout = {
+        "header": True,
+        "main": True,
+        "left_sidebar": True,
+        "right_sidebar": False,
+        "footer": True,
+    }
+
+    context = {
+        "site_info": site_info,
+        "custom_menus": custom_menus,
+        "template_layout": template_layout,
+    }
+
+    return context
