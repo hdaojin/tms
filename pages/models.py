8cc36a8d2d63921aea0c135927d3d6dbabb1d6c5
hdaojin <hdaojin@hotmail.com>
2025-08-25 19:36:08 +0800

    优化了登录界面，统一了风格

--- pages/models.py
+++ pages/models.py
@@ -7,8 +7,8 @@ class Page(models.Model):
     TEMPLATE_CHOICES = (
         ('default', '默认'),
         ('no_sidebar', '无侧边栏'),
-        ('no_header_sidebar', '无头部和侧边栏'),
-        ('no_header_sidebar_footer', '无头部、侧边栏和底部'),
+        ('no_header_sidebar', '无头部、无侧边栏'),
+        ('no_header_sidebar_footer', '无头部、无侧边栏、无底部'),
         # 可根据需要添加更多模板
     )
     title = models.CharField(max_length=255, verbose_name="标题")
@@ -18,7 +18,8 @@ class Page(models.Model):
         max_length=50,
         choices=TEMPLATE_CHOICES,
         default='default',
-        verbose_name='页面模板'
+        verbose_name='页面模板',
+        help_text="选择页面的布局模板, 例如：是否显示侧边栏、头部和底部等。'首页'必须选择'无头部、无侧边栏、无底部'"
     )
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
