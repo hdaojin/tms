ceb5132b60391b115615f26ec7417b8269e636ea
hdaojin <hdaojin@hotmail.com>
2026-05-16 15:24:53 +0800

    optimize:删除无用的文件和代码

--- meeting/tests.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-
-class MeetingUrlTests(TestCase):
-	def test_meeting_list_is_mounted_at_app_root(self):
-		self.assertEqual(reverse('meeting:meeting_list'), '/meeting/')
